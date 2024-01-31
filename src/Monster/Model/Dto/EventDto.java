18c3516377e281df0b1348a8883f9a041432d08a
jang333 <tidls27@naver.com>
2024-01-31 17:53:52 +0900

    240131 EventDto 작성

--- src/Monster/Model/Dto/EventDto.java
+++ src/Monster/Model/Dto/EventDto.java
@@ -1,4 +1,63 @@
 package Monster.Model.Dto;
 
 public class EventDto {
-}
+
+    //1. 필드
+    private int eno;
+    private String eimg;
+    private int statUp;
+    private int statDown;
+
+    //2.생성자
+    EventDto(){}
+
+    public EventDto(int eno, String eimg, int statUp, int statDown) {
+        this.eno = eno;
+        this.eimg = eimg;
+        this.statUp = statUp;
+        this.statDown = statDown;
+    }
+
+    //3.메소드
+    public int getEno() {
+        return eno;
+    }
+
+    public void setEno(int eno) {
+        this.eno = eno;
+    }
+
+    public String getEimg() {
+        return eimg;
+    }
+
+    public void setEimg(String eimg) {
+        this.eimg = eimg;
+    }
+
+    public int getStatUp() {
+        return statUp;
+    }
+
+    public void setStatUp(int statUp) {
+        this.statUp = statUp;
+    }
+
+    public int getStatDown() {
+        return statDown;
+    }
+
+    public void setStatDown(int statDown) {
+        this.statDown = statDown;
+    }
+
+    @Override
+    public String toString() {
+        return "EventDto{" +
+                "eno=" + eno +
+                ", eimg='" + eimg + '\'' +
+                ", statUp=" + statUp +
+                ", statDown=" + statDown +
+                '}';
+    }
+}//class e
