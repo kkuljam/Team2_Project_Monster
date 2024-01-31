f3b13c0f176a58d656032e97a996c78a632bbae8
kkuljam <adgrvet13@naver.com>
2024-01-31 17:53:58 +0900

    memberDto 작성

--- src/Monster/Model/Dto/MemberDto.java
+++ src/Monster/Model/Dto/MemberDto.java
@@ -1,4 +1,73 @@
 package Monster.Model.Dto;
 
 public class MemberDto {
+    //1. 필드
+    private int mno;
+    private String mid;
+    private String mpw;
+    private String mphone;
+    private String mname;
+
+    //2. 생성자
+    public MemberDto() {
+    }
+
+    public MemberDto(int mno, String mid,String mpw,String mphone,String mname) {
+        this.mno = mno;
+        this.mid = mid;
+        this.mpw = mpw;
+        this.mphone = mphone;
+        this.mname = mname;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberDto{" +
+                "mno=" + mno +
+                ", mid='" + mid + '\'' +
+                ", mpw='" + mpw + '\'' +
+                ", mphone='" + mphone + '\'' +
+                ", mname='" + mname + '\'' +
+                '}';
+    }
+
+    public int getMno() {
+        return mno;
+    }
+
+    public void setMno(int mno) {
+        this.mno = mno;
+    }
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    public String getMpw() {
+        return mpw;
+    }
+
+    public void setMpw(String mpw) {
+        this.mpw = mpw;
+    }
+
+    public String getMphone() {
+        return mphone;
+    }
+
+    public void setMphone(String mphone) {
+        this.mphone = mphone;
+    }
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
 }
