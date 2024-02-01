6a86ad6d8d3f48ec2d887e0a2abcadc1c1e8b9fa
challenger1736 <challenger1736@gmail.com>
2024-02-01 16:00:35 +0900

    회원목록불러오는중 2-01 16:00

--- src/Monster/Controller/AdminController.java
+++ src/Monster/Controller/AdminController.java
@@ -1,4 +1,15 @@
 package Monster.Controller;
 
+import Monster.View.AdminView;
+
 public class AdminController {
+    private AdminController(){}
+    private static AdminController adminController= new AdminController();
+
+    public static  AdminController getInstance(){return adminController;}
+
+    // 모든회원정보 출력
+    //public String
+
+
 }
