2394133b95b2fd0daf3de37d9355632a56db6830
kkuljam <adgrvet13@naver.com>
2024-02-08 12:12:22 +0900

    02-08 12:12 수정

--- src/Monster/View/MonsterPlayView.java
+++ src/Monster/View/MonsterPlayView.java
@@ -13,17 +13,16 @@ public class MonsterPlayView {
     private static MonsterPlayView monsterView=new MonsterPlayView();
     public static MonsterPlayView getInstance(){return monsterView;}
 
-    int mno= MemberController.getInstance().logMno;// 로그인 되어있는 회원번호
+
     public void run(){
         Scanner scanner=new Scanner(System.in);
-
+        int mno= MemberController.getInstance().logMno;// 로그인 되어있는 회원번호
         while (true){
             boolean result = MonsterController.getInstance().monsterExistence(mno); //몬스터 보유 여부 메소드
             if(result){ // 몬스터가 없으면 알 생성으로 보내기
                 MonsterView.getInstance().monsterCreate(mno); // 몬스터 알 생성하는 메소드
-            }else {
-                MonsterView.getInstance().monsterGameImg(mno); // 회원번호에 맞는 몬스터 출력
             }
+            MonsterView.getInstance().monsterGameImg(mno); // 회원번호에 맞는 몬스터 출력
 
             System.out.println("0. 로그아웃");
             ArrayList<EventDto>eventDtos=MonsterController.getInstance().eventPrint(new EventDto()); //육성 기능 카테고리 배열
